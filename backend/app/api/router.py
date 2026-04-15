eb02433ab4e937b1cd25b05192c12435cbaf6c08
rheubido-spec <rheubido@gmail.com>
2026-04-14 21:31:16 -0400

    Update router.py

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.routes import admin, auth, campaigns, characters, dice, forum, maps, parties, reference, tracker
+from app.api.routes import admin, auth, campaigns, characters, dice, forum, maps, parties, reference
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
@@ -11,4 +11,4 @@ api_router.include_router(forum.router)
 api_router.include_router(reference.router)
 api_router.include_router(admin.router)
 api_router.include_router(maps.router)
-api_router.include_router(tracker.router)
+
